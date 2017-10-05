9cd428a34708adde0405579bfa36801f45c2537d
moltenshark <alex@berez.com>
2017-10-05 14:41:29 -0500

    more styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,25 +33,25 @@ $(function(){
 		$("#characterSelection").append("<h3>CHOOSE YOUR CHARACTER</h3>")
 		$("#listHome").html("<div id='characterList'></div>");
 		//Greedo
-		$("#characterList").html('<div id="characterGreedo" class="character notChosen"><img src="assets/images/greedo copy.jpg" /></div>');
+		$("#characterList").html('<div id="characterGreedo" class="character notChosen"><img src="assets/images/greedo copy.jpg" /><div class="overlay-target overlay"></div><div class="damage-effect"></div></div>');
 		$("#characterGreedo").data('stats', greedo);
 		$("#characterGreedo").append("<span class='characterName'>Greedo</span>");
 		$("#characterGreedo").append("<span class='characterHealth'>Health:<span class='healthNumber green'>" + greedo.healthPoints + "</span></span>");
 		$("#characterGreedo").append("<div class='infoDiv'><span class='characterInfo align-middle'>Poor aim, worse luck.</span></div>");
 		//IG-88
-		$("#characterList").append('<div id="characterIg88" class="character notChosen"><img src="assets/images/ig-88 copy.jpg" /></div>');
+		$("#characterList").append('<div id="characterIg88" class="character notChosen"><img src="assets/images/ig-88 copy.jpg" /><div class="overlay-target overlay"></div><div class="damage-effect"></div></div>');
 		$("#characterIg88").data('stats', ig88);
 		$("#characterIg88").append("<span class='characterName'>IG-88</span>");
 		$("#characterIg88").append("<span class='characterHealth'>Health:<span class='healthNumber green'>" + ig88.healthPoints + "</span></span>");
 		$("#characterIg88").append("<div class='infoDiv'><span class='characterInfo'>Droid extra with a killer backstory.</span></div>");
 		//Sarlacc
-		$("#characterList").append('<div id="characterSarlacc" class="character notChosen"><img src="assets/images/sarlacc copy.png" /></div>');
+		$("#characterList").append('<div id="characterSarlacc" class="character notChosen"><img src="assets/images/sarlacc copy.png" /><div class="overlay-target overlay"></div><div class="damage-effect"></div></div>');
 		$("#characterSarlacc").data('stats', sarlacc);
 		$("#characterSarlacc").append("<span class='characterName'>Sarlacc</span>");
 		$("#characterSarlacc").append("<span class='characterHealth'>Health:<span class='healthNumber green'>" + sarlacc.healthPoints + "</span></span>");
 		$("#characterSarlacc").append("<div class='infoDiv'><span class='characterInfo'>Unsung hero of <i>Return of the Jedi</i>.</span></div>");
 		//Tauntaun
-		$("#characterList").append('<div id="characterTauntaun" class="character notChosen"><img src="assets/images/tauntaun copy.jpg" /></div>');
+		$("#characterList").append('<div id="characterTauntaun" class="character notChosen"><img src="assets/images/tauntaun copy.jpg" /><div class="overlay-target overlay"></div><div class="damage-effect"></div></div>');
 		$("#characterTauntaun").data('stats', tauntaun);
 		$("#characterTauntaun").append("<span class='characterName'>Tauntaun</span>");
 		$("#characterTauntaun").append("<span class='characterHealth'>Health:<span class='healthNumber green'>" + tauntaun.healthPoints + "</span></span>");
@@ -60,7 +60,6 @@ $(function(){
 
 	function resetGame() {
 		$("#characterAttack").empty();
-		$("#characterDefend").empty();
 		$("#characterFight").empty();
 		$('#fightButton').empty();
 		attackActual = 0;
@@ -86,13 +85,17 @@ $(function(){
 		else if ($('#characterFight').is(':empty')) {
 			$("#characterFight").append("<h3>DEFENDING ENEMY</h3>");
 			$('#characterFight').append(this);
+			$('#characterList .overlay-target').removeClass("overlay");
 			defendHealth = $("#characterFight .character").data("stats").healthPoints;
 			if ($('#fightButton').is(':empty')) {
 				var fightBtn = $("<button>");
 		        $(fightBtn).addClass("btn btn-lg btn-danger fight-button");
 		        $(fightBtn).text("FIGHT!");
 				$("#fightButton").append(fightBtn);
-			}	
+			}
+			if (charactersDefeated == 2) {
+				$("#characterDefend").empty();
+			} 
 		}
     });
 
@@ -100,13 +103,18 @@ $(function(){
 		var attackData = $("#characterAttack .character").data("stats");
 		var defendData = $("#characterFight .character").data("stats");
 		attackHealth = attackHealth - defendData.counterAttack;
+		$("#characterAttack .damage-effect").fadeIn("fast");
+		$("#characterAttack .damage-effect").fadeOut("fast");
 		$("#characterAttack .healthNumber").html(attackHealth);
 		if (attackHealth / attackData.healthPoints <= 0.5) {
 			$("#characterAttack .healthNumber").removeClass('green').addClass('red');
 		}
 		attackActual = attackActual + attackData.attackPower;
 		defendHealth = defendHealth - attackActual;
+		$("#characterFight .damage-effect").fadeIn("fast");
+		$("#characterFight .damage-effect").fadeOut("fast");
 		$("#characterFight .healthNumber").html(defendHealth);
+
 		if (defendHealth / defendData.healthPoints <= 0.5) {
 			$("#characterFight .healthNumber").removeClass('green').addClass('red');
 		}
@@ -123,6 +131,7 @@ $(function(){
 				resetGame();
 			}
 			else {
+				$('#characterList .overlay-target').addClass("overlay");
 				alert("Choose a new defender!")
 			}
 		}
