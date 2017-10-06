7756dd2b30c0c44178aad32569b2988aacda4acf
moltenshark <alex@berez.com>
2017-10-06 15:43:54 -0500

    better button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -62,7 +62,7 @@ $(function(){
 		$('#gameButton').empty();
 		var resetBtn = $("<button>");
         	$(resetBtn).addClass("btn btn-lg reset-button");
-        	$(resetBtn).text("PLAY AGAIN?");
+        	$(resetBtn).html("PLAY<br>AGAIN?");
         	$("#gameButton").append(resetBtn);
 	}
 	
